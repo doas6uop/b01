e347387c470e2b58c0136a2f1d01a5da5771229f
ghost <83461045+doas6uop@users.noreply.github.com>
2023-05-12 17:19:00 +0900

    :zap: QBaseEntity, QBoard 생성

---
+++ src/main/java/org/zerock/b01/domain/QBoard.java
@@ -0,0 +1,43 @@
+package org.zerock.b01.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = -2096181413L;
+
+    public static final QBoard board = new QBoard("board");
+
+    public final NumberPath<Long> bno = createNumber("bno", Long.class);
+
+    public final StringPath content = createString("content");
+
+    public final StringPath title = createString("title");
+
+    public final StringPath writer = createString("writer");
+
+    public QBoard(String variable) {
+        super(Board.class, forVariable(variable));
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBoard(PathMetadata metadata) {
+        super(Board.class, metadata);
+    }
+
+}
+
